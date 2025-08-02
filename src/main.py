1653b138fa37363d3060d2494457ee31f885b8e0
John Colton <jcolton96@gmail.com>
2025-08-02 20:51:26 +0100

    feat: Working script to query items on Herman Miller page

--- src/main.py
+++ src/main.py
@@ -1,28 +1,89 @@
-import requests
+import re
+import sys
+import logging
+from urllib.request import urlopen
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel("DEBUG")
+console_handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    "{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
 
 def get_item_price(url: str):
     """Return the price of an item on a website"""
-    print(url)
-    r = requests.get(url)
-    print(r.status_code)
-    content = r.text
-    return content
+    logger.info(f"Searching for item price on page: {url}")
+    page = urlopen(url)
+
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    return html
 
 
 def main():
     base_url = "https://ukstore.hermanmiller.com/products"
+
+    chair_variants = [
+        {
+            "model": "aeron",
+            "size": "medium",
+            "arms": "standard_fully_adjustable",
+            "back_support": "fixed_posturefit",
+            "chair_id": "42502223036569"
+        }
+    ]
+
     chair_name = "aeron-graphite-standard-office-chair"
-    chair_variant = "42502223036569" # Herman Miller Aeron - Medium - Fully Leather Adjustable
+    model = "aeron"
+    size = "medium"
+    arms = "standard_fully_adjustable"
+    back_support = "fixed_posturefit"
+
+    matches = [chair for chair in chair_variants if
+               chair["model"] == model and
+               chair["size"] == size and
+               chair["arms"] == arms and
+               chair["back_support"] == back_support
+    ]
+    if matches:
+        chair_variant = matches[0]["chair_id"]
+    else:
+        logger.critical("No ID found for chair details selected. Please try different values.")
+        sys.exit()
 
     url = f"{base_url}/{chair_name}?variant={chair_variant}"
 
     response = get_item_price(url)
-    print(response)
-    print(type(response))
 
-    x = response.find("Add to Basket")
-    print(x)
+
+    # TODO Seperate this logic into own function
+    # Search entire HTML content for section with price
+    # Within this section grab exact part with price
+    # Convert price into GBP
+    # TODO Add error handling for not finding text in HTML
+    start_index = response.find("<span id=\"addToCartText\">")
+    end_index = start_index + response[start_index:].find("</span>")
+    price_section = response[start_index:end_index]
+
+    pattern = r'data-total-price="(\d+)"'
+
+    match = re.search(pattern, price_section)
+    if match:
+        price = match.group(1)  # This returns just the number
+    else:
+        logger.critical("Item price not found.")
+        sys.exit()
+
+
+    gbp_price = int(price) / 100
+    print(f"£{gbp_price}")
 
 
 if __name__ == "__main__":
